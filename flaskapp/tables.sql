30cc74c7200c4c93022b9ab1778fdf8543d46aa7
zakiayoubi <122570765+zakiayoubi@users.noreply.github.com>
2024-03-14 15:23:43 +0000

    created the onationinflow

--- flaskapp/tables.sql
+++ flaskapp/tables.sql
@@ -1,8 +1,11 @@
 -- https://www.postgresqltutorial.com/
 
-CREATE TABLE othertable (
-    otherid integer PRIMARY KEY,
-    data varchar
+CREATE TABLE donationInflow (
+    donationId SERIAL PRIMARY KEY,
+    donorName VARCHAR(100),
+    category VARCHAR(50),
+    donationDate DATE,
+    amount NUMERIC(12, 2)
 );
 
 CREATE TABLE example (
@@ -21,3 +24,11 @@ CREATE TABLE Donation_Ouflow (
     doneeName varchar(40),
     category varchar(30)
 );
+-- CREATE TABLE example (
+--     id serial PRIMARY KEY,
+--     username varchar(32) UNIQUE,
+--     description varchar(255) NOT NULL,
+--     fkey_other integer REFERENCES othertable(otherid), 
+--     isInt smallint NOT NULL DEFAULT(0),
+--     isBool boolean
+-- );
