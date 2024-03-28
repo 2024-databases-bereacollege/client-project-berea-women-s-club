d5c219a82900cc95422b7074d54b207d94cf863e
Brothers9152 <122465834+Brothers9152@users.noreply.github.com>
2024-03-27 23:48:39 -0400

    Update models.py
    Added Tables!

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -14,9 +14,64 @@ class OtherTable (baseModel):
     otherid = PrimaryKeyField()
     data = CharField(null=False)
 
-class Example (baseModel):
-    username = CharField(32,unique=True)
-    description = CharField(255)
-    other = ForeignKeyField(OtherTable)
-    isInt = BooleanField(default=0)
-    isBool = BooleanField()
+class member (baseModel):
+    memberID = PrimaryKeyField()
+    firstName = CharField(255)
+    lastName = CharField(255)
+    email = CharField(255)
+    phoneNumber = IntegerField(10)
+    streetName = CharField(255)
+    city = CharField(255)
+    usState = CharField(255)
+    zipCode = IntegerField(9)
+    dateOfBirth = DateField()
+    dateJoined = DateField()
+    paidDues = BooleanField()
+    
+
+
+class donationInflow (baseModel):
+    donationInflowID = PrimaryKeyField()
+    category = CharField(100)
+    amount = IntergerField(900)
+    donationDate = DateField()
+    memberID = ForeignKeyField('self', null=True, backref='member')
+
+
+
+class donationOutflow (baseModel):
+    donationInflowID = PrimaryKeyField()
+    category = CharField(100)
+    amount = IntergerField(900)
+    donationDate = DateField()
+    organizationID = ForeignKeyField('self', null=True, backref='organization')
+
+
+class event (baseModel):
+    eventID = PrimaryKeyField()
+    eventName = CharField(255)
+    eventLocation = CharField(255)
+    streetName = CharField(255)
+    city = CharField(255)
+    usState = CharField(255)
+    zipCode = IntegerField(9)
+    eventDate = DateField()
+    amountRaised = IntegerField(999)
+    eventCost = IntegerField(999)
+    eventType = CharField(255)
+
+class host (baseModel):
+    primary_key = CompositeKey(eventID, memberID)
+    memberID = ForeignKeyField('self', null=True, backref='member')
+    eventID = ForeignKeyField('self', null=True, backref='event')
+
+class organization(baseModel):
+    organizationID = PrimaryKeyField()
+    organizationName = CharField(255)
+    email = CharField(255)
+    phoneNumber = IntegerField(10)
+    streetName = CharField(255)
+    city = CharField(255)
+    usState = CharField(255)
+    organizationType = CharField(255)
+  
