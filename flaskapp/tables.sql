7190661b40724b2b2e557b39fce5e5ebd41826f0
zakiayoubi <122570765+zakiayoubi@users.noreply.github.com>
2024-03-28 00:09:56 +0000

    imporve the tables

--- flaskapp/tables.sql
+++ flaskapp/tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE member (
     zipCode INT,
     dateOfBirth DATE,
     dateJoined DATE DEFAULT CURRENT_DATE,
-    paidDues BOOLEAN,
+    paidDues BOOLEAN
 );
 
 CREATE TABLE donationInflow (
@@ -20,7 +20,7 @@ CREATE TABLE donationInflow (
     category VARCHAR(100),
     amount NUMERIC(12, 2),
     donationDate DATE DEFAULT CURRENT_DATE,
-    FOREIGN KEY (organizationID) REFERENCES organization(organizationID),
+    FOREIGN KEY (organizationID) REFERENCES organization(organizationID)
 );
 
 CREATE TABLE donation_Ouflow (
@@ -28,24 +28,29 @@ CREATE TABLE donation_Ouflow (
     donationDate DATE DEFAULT CURRENT_DATE,
     amount NUMERIC(12, 2),
     category varchar(100),
-    FOREIGN KEY (organizationID) REFERENCES organization(organizationID),
+    FOREIGN KEY (organizationID) REFERENCES organization(organizationID)
 );
 
 CREATE TABLE event (
     eventID SERIAL PRIMARY KEY,
     eventName VARCHAR(150),
+    eventLocation VARCHAR(150),
+    streetName VARCHAR(150),
+    city VARCHAR(50),
+    usState VARCHAR(50),
+    zipCode INT,
     eventDate DATE DEFAULT CURRENT_DATE,
     amountRaised NUMERIC(12, 2),
     eventCost NUMERIC(12, 2),
-    eventType varchar(100),
-    FOREIGN KEY (memberID) REFERENCES member(memberID),
+    eventType varchar(100)
 );
 
 CREATE TABLE host (
-    hostID SERIAL PRIMARY KEY,
-    eventlocation VARCHAR(150),
+    eventID INT,
+    memberID INT,
+    PRIMARY KEY (eventID, memberID),
     FOREIGN KEY (eventID) REFERENCES event(eventID),
-    FOREIGN KEY (memberID) REFERENCES member(memberID),
+    FOREIGN KEY (memberID) REFERENCES member(memberID)
 );
 
 CREATE TABLE organization (
@@ -57,6 +62,6 @@ CREATE TABLE organization (
     city VARCHAR(50),
     usState VARCHAR(50),
     zipCode INT,
-    organizationType varchar (100),
+    organizationType varchar (100)
 );
 
